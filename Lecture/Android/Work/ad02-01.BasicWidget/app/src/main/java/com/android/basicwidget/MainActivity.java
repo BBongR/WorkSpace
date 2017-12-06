58674c5f324f77b8177d613a58889fbdcbf21e61
BBongR <suv1214@naver.com>
2017-12-06 18:11:56 +0900

    1

---
+++ Lecture/Android/Work/ad02-01.BasicWidget/app/src/main/java/com/android/basicwidget/MainActivity.java
@@ -0,0 +1,87 @@
+package com.android.basicwidget;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 이벤트 핸들러 설정, 선언. 찾기. 설정.
+        Button button1 = null;
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), RadioActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button button2 = null;
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), WidhtButtonActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button button3 = null;
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), HeightButtonActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button button4 = null;
+        button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), ColorButtonActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button button5 = null;
+        button5 = findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MarginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button button6 = null;
+        button6 = findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), PaddingActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button button7 = null;
+        button7 = findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), VisibleActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
