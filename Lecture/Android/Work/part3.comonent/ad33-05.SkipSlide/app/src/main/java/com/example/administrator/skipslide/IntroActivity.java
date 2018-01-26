14ec9a561dbfa83d6696d0ba3a51e3f158442926
bbongr <suv1214@naver.com>
2018-01-26 18:11:20 +0900

    fds

--- Lecture/Android/Work/part3.comonent/ad33-05.SkipSlide/app/src/main/java/com/example/administrator/skipslide/IntroActivity.java
+++ Lecture/Android/Work/part3.comonent/ad33-05.SkipSlide/app/src/main/java/com/example/administrator/skipslide/IntroActivity.java
@@ -1,7 +1,6 @@
 package com.example.administrator.skipslide;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
